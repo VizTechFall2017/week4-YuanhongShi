94f2938ef65a0503b57daa3c00075ca0a166c2b3
YuanhongShi <31866504+YuanhongShi@users.noreply.github.com>
2017-10-08 22:46:11 -0700

    Set the structuer for the dataset

--- PS01/index.js
+++ PS01/index.js
@@ -1,3 +1,44 @@
 var svg = d3.select('svg');
+var defs = svg.append('defs');
 
-/* Your code goes here */
+defs.append('pattern')
+    .attr('id','bg')
+    .attr('patternUnits', 'userSpaceOnUse')
+    .attr('width', 1250)
+    .attr('height', 700)
+    .append('image')
+    .attr('xlink:href', 'Background_Single.png')
+    .attr('width', 1250)
+    .attr('height', 700)
+    .attr('x', 0)
+    .attr('y', 0);
+
+console.log('here');
+
+svg.append('rect')
+    .attr('width', 1250)
+    .attr('height', 700)
+    .attr('fill', 'url(#bg)');
+
+
+d3.csv('./PathData.csv', function(dataIn){
+    svg.selectAll('circle')
+        .data(dataIn)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d){
+            return d.x;
+        })
+        .attr('cy', function(d){
+            return d.y;
+        })
+        .attr('r', function(d){
+            return d.r;
+        })
+        .attr('fill', function(d){
+            console.log(d.fill);
+            return d.fill;
+
+        })
+
+});
--- makeButton/index.js
+++ makeButton/index.js
@@ -1,11 +1,49 @@
-var svg = d3.select('svg');
+var svg = d3.select('svg')
+    .append('g')    //g means group, treat the svg and its context as a group
+    .attr('transform', 'translate(100,100)');
 
 /* Your code goes here */
 
 
+d3.csv('./data.csv', function(dataIn){
+
+    //console.log(dataIn);
+    svg.selectAll('circle')//grap the container of circle
+        .data(dataIn)
+        .enter()//enter the data to the container
+        .append('circle')
+        .attr('cx',function(d){
+            return scaleX(d.x);
+        })
+        .attr('cy',function(d){
+            return scaleY(d.y);
+        })
+        .attr('r',10);
+    svg.append('g')
+        .attr('transform', 'translate(0,400)')
+        .call(d3.axisBottom(scaleX));
+    svg.append('g')
+
+        .call(d3.axisLeft(scaleY));
+
+    svg.append('text')
+        .attr('x', 300)
+        .attr('y', 450)
+        .attr('font-size', 16)
+        .text('x axis title');
+
+    svg.append('text')
+        .attr('x', -200)
+        .attr('y', -50)
+        .attr('font-size', 16)
+        .text('y axis title')
+        .attr('transform', 'rotate(270)');
+});
+
+var scaleX = d3.scaleLinear().domain([0,400]).range([0,600]); //cause d3.scaleLinear() is a built in function
+//so it is start from var;
+var scaleY = d3.scaleLinear().domain([500,0]).range([0,400]);
+
 function buttonClicked(){
-    //do something
     console.log('here');
 }
-
-
